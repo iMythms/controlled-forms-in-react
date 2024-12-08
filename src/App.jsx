5a7ac89630fa536ee45d49b62f8f26ac2204903f
iMythms <maythams.149@gmail.com>
2024-12-08 19:11:56 +0300

    refactor: update index.html structure, enhance App component with controlled forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 const App = () => {
+	const [title, setTitle] = useState('The full name will appear here.')
+
+	const [formData, setFormData] = useState({
+		firstName: '',
+		lastName: '',
+	})
+
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value })
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		setTitle(`Your name is ${formData.firstName} ${formData.lastName}`)
+		setFormData({
+			firstName: '',
+			lastName: '',
+		})
+	}
+
 	return (
 		<div>
-			<h1>Hello World!</h1>
+			<h2>{title}</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="firstName">First Name: </label>
+					<input
+						id="firstName"
+						name="firstName"
+						value={formData.firstName}
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlFor="lastName">Last Name: </label>
+					<input
+						id="lastName"
+						name="lastName"
+						value={formData.lastName}
+						onChange={handleChange}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	)
 }
